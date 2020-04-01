5ad004c0338686c3e17f6097cbdd7598721df141
CHALAL <abdellatifchalal44@gmail.com>
2020-04-01 11:12:13 +0200

    app laravel avec le fichier sql et le rapport

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,138 @@
+<?php
+    
+namespace App\Http\Controllers\Admin;
+    
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Spatie\Permission\Models\Role;
+use DB;
+use Hash;
+    
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $keyword = $request->get('search');
+        $perPage = 25;
+
+        if (!empty($keyword)) {
+            $user = User::where('name','LIKE',"%$keyword%")->paginate($perPage);
+        } else {
+            $user = User::paginate($perPage);
+        }
+
+        return view('admin.user.index', compact('user'));
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::pluck('name','name')->all();
+        return view('admin.user.create',compact('roles'));
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|same:confirm-password',
+            'roles' => 'required'
+        ]);
+    
+        $input = $request->all();
+        $input['password'] = Hash::make($input['password']);
+    
+        $user = User::create($input);
+        $user->assignRole($request->input('roles'));
+    
+        return redirect('admin/user')->with('flash_message', 'User added!');
+
+    }
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('admin.user.show',compact('user'));
+    }
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('admin.user.edit', compact('user'));
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,'.$id,
+            'password' => 'same:confirm-password',
+            'roles' => 'required'
+        ]);
+    
+        $input = $request->all();
+        if(!empty($input['password'])){ 
+            $input['password'] = Hash::make($input['password']);
+        }else{
+            $input['password'] = User::find($id)->password;
+        }
+    
+        $user = User::find($id);
+        $user->update($input);
+        DB::table('model_has_roles')->where('model_id',$id)->delete();
+    
+        $user->assignRole($request->input('roles'));
+    
+        return redirect('admin/user')->with('flash_message', 'User updated!');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::destroy($id);
+
+        return redirect('admin/user')->with('flash_message', 'User deleted!');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware'=>['role:super-admin','auth']],function(){
+	Route::resource('admin/permission', 'Admin\\PermissionController');
+	Route::resource('admin/role', 'Admin\\RoleController');
+	Route::resource('admin/user', 'Admin\\UserController');
+});
+
+Route::group(['middleware'=>['auth']],function(){
+	Route::view('/admin','admin.dashboard');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
